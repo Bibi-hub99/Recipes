8bccc708e5c68b6fa1f7b2bf06279ddb11e5e937
Bibi-hub99 <anxumalo000@gmail.com>
2025-04-25 03:35:55 +0200

    fix

--- frontend-app/dist/assets/mealArea-BfbIVASo.js
+++
@@ -1,1 +0,0 @@
-import{d as t,j as s}from"./index-Dcn6LiOt.js";function n(){const[e,r]=t();return s.jsx("h2",{children:e.strArea})}export{n as default};
--- frontend-app/dist/assets/mealCategory-Cidg2U-N.js
+++
@@ -1,1 +0,0 @@
-import{d as t,j as s}from"./index-Dcn6LiOt.js";function a(){const[e,r]=t();return s.jsx("h2",{children:e.strCategory})}export{a as default};
